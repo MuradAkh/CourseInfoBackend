65ef3543f01223e9a3347e494bd3d9649f8fce05
Murad Akhundov <muradakhdev@gmail.com>
2020-07-12 13:55:39 -0400

    fix port

--- main.go
+++ main.go
@@ -9,9 +9,13 @@ func main() {
 	a := App{}
 	pwd, exists1 := os.LookupEnv("REDIS_PASSWORD")
 	addrs, exists2 := os.LookupEnv("REDIS_ENDPOINT")
+	port, noport := os.LookupEnv("PORT")
+	if noport || port == "" {
+		port = "8080"
+	}
 	if exists1 && exists2 {
 		a.Initialize(pwd, addrs)
-		a.Run(":8080")
+		a.Run(":" + port)
 	}else{
 		_ = fmt.Errorf("needs env var REDIS_PASSWORD")
 	}
